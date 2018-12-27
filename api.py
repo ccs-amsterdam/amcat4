935a61c005930ad06e2efdc578b618d34f806ac1
Wouter van Atteveldt <wouter@vanatteveldt.com>
2018-12-27 03:52:02 +0100

    First API functions: get_token, create / list project

---
+++ api.py
@@ -0,0 +1,65 @@
+from flask import request, jsonify, Flask, g
+from flask_httpauth import HTTPBasicAuth, HTTPTokenAuth, MultiAuth
+
+from http import HTTPStatus
+
+import auth
+from auth import create_token
+from elastic import list_projects, create_project
+
+app = Flask(__name__)
+
+basic_auth = HTTPBasicAuth()
+token_auth = HTTPTokenAuth()
+multi_auth = MultiAuth(basic_auth, token_auth)
+
+
+@basic_auth.verify_password
+def verify_password(username, password):
+    if auth.verify_user(username, password):
+        g.email = username
+        return True
+
+
+@token_auth.verify_token
+def verify_token(token):
+    email = auth.verify_token(token)
+    if email is not None:
+        g.email = email
+        return True
+
+
+@app.route("/auth/token/", methods=['GET'])
+@multi_auth.login_required
+def get_token():
+    """
+    Create a new token for the authenticated user
+    """
+    token = create_token(g.email)
+    return jsonify({"token": token})
+
+
+@app.route("/projects/", methods=['GET'])
+@multi_auth.login_required
+def project_list():
+    """
+    List projects from this server
+    """
+    result = [{"name": name} for name in list_projects()]
+    return jsonify(result)
+
+
+@app.route("/projects/", methods=['POST'])
+@multi_auth.login_required
+def project_create():
+    """
+    Create a new project
+    """
+    data = request.get_json(force=True)
+    name = data['name']
+    create_project(name)
+    return jsonify({"name": name}), HTTPStatus.CREATED
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
