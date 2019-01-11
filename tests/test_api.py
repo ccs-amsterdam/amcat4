cbe4401e46cf89a485115fc818657533d3ff493b
Wouter van Atteveldt <wouter@vanatteveldt.com>
2019-01-11 14:46:23 +0100

    Create users in API unit test

--- tests/test_api.py
+++ tests/test_api.py
@@ -5,21 +5,32 @@ import string
 from nose.tools import assert_equal, assert_in, assert_not_in
 
 from amcat4.__main__ import app
-from amcat4.auth import verify_token
+from amcat4.auth import verify_token, create_user, delete_user, ROLE_ADMIN
 
 C = None
+_TEST_ADMIN = '__test__admin__'+ ''.join(random.choices(string.ascii_lowercase, k=32))
+_TEST_USER = '__test__user__'+ ''.join(random.choices(string.ascii_lowercase, k=32))
 
 
 def setup_module():
     global C
     C = app.test_client()
     C.testing = True
+    create_user(_TEST_ADMIN, "password", roles=ROLE_ADMIN, check_email=False)
+    create_user(_TEST_USER, "password", check_email=False)
 
 
-def _request(url, method='get', **kwargs):
+
+def teardown_module():
+    delete_user(_TEST_ADMIN, ignore_missing=True)
+    delete_user(_TEST_USER, ignore_missing=True)
+
+
+def _request(url, method='get', user=_TEST_USER, password="password", **kwargs):
     request_method = getattr(C, method)
-    auth = "Basic {}".format(base64.b64encode(b"admin:admin").decode('ascii'))
-    result = request_method(url, headers={"Authorization": auth}, **kwargs)
+    cred = ":".join([user, password])
+    auth = base64.b64encode(cred.encode("ascii")).decode('ascii')
+    result = request_method(url, headers={"Authorization": "Basic {}".format(auth)}, **kwargs)
     if result.status_code not in {200, 201}:
         assert_in(result.status_code, {200, 201}, result.get_data(as_text=True))
     return result
@@ -37,7 +48,7 @@ def test_get_token():
     assert_equal(C.get('/auth/token/').status_code, 401, "Getting project list should require authorization")
     r = _get('/auth/token/')
     token = r.json['token']
-    assert_equal(verify_token(token), 'admin')
+    assert_equal(verify_token(token), _TEST_USER)
 
 
 def test_project():
