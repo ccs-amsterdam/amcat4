66074aa56dd5d340428f568e2811a64d3cde862c
Farzam <farzam.fanni@gmail.com>
2021-12-03 09:16:38 +0100

    added dependency on the annotator-backend

--- amcat4/api/__init__.py
+++ amcat4/api/__init__.py
@@ -1,18 +1,21 @@
-
 from flask import Flask
 from flask_cors import CORS
 
 from amcat4annotator import app_annotator
+from amcat4annotator import app_annotator_auth
+
 from amcat4.api.common import MyJSONEncoder
 from amcat4.api.query import app_query
 from amcat4.api.users import app_users
 from amcat4.api.index import app_index
 
 app = Flask(__name__)
+
 app.json_encoder = MyJSONEncoder
 CORS(app)
 app.register_blueprint(app_index)
 app.register_blueprint(app_query)
 app.register_blueprint(app_users)
-app.register_blueprint(app_annotator)
 
+app.register_blueprint(app_annotator)
+app.register_blueprint(app_annotator_auth)
