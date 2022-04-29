d38b21b11b797e3b98dbb73251ee5dd869e9842a
Wouter van Atteveldt <wouter@vanatteveldt.com>
2022-04-29 10:08:45 +0200

    Remove docs, fix main / add cors

--- amcat4/api/__init__.py
+++ amcat4/api/__init__.py
@@ -8,11 +8,18 @@ from fastapi import FastAPI
 from amcat4.api.index import app_index
 from amcat4.api.query import app_query
 from amcat4.api.users import app_users
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(app_users)
 app.include_router(app_index)
 app.include_router(app_query)
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 # "Plugins"
 #app.register_blueprint(app_annotator, url_prefix='/annotator')
