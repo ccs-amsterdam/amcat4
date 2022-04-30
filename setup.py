e6e306c0e6d3c6bb3b59e7615c8664f528d6eef9
Wouter van Atteveldt <wouter@vanatteveldt.com>
2022-04-30 12:58:03 +0200

    pypi version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name="amcat4",
-    version="0.08",
+    version="0.09",
     description="API for AmCAT4 Text Analysis",
     author="Wouter van Atteveldt",
     author_email="wouter@vanatteveldt.com",
