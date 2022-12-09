6e68900294de0126f49cd4ad3e7f77daf643f9af
Wouter van Atteveldt <wouter@vanatteveldt.com>
2022-12-09 17:18:13 +0100

    config update

--- amcat4/config.py
+++ amcat4/config.py
@@ -1,31 +1,38 @@
 """
 AmCAT4 Configuration
 
-We generally read configuration from 3 sources:
-- Environment variables
+We read configuration from 3 sources, in order of precedence (higher is more priority)
 - Command line flags (from __main__.py)
-- A .env file, either in the current working directory or in a location specified
+- Environment variables (also read from .env in the working directory)
+- A amcat.env file, either in the current working directory or in a location specified
   by the --config-file flag or AMCAT4_CONFIG_FILE environment variable
 """
-
+import functools
+from pathlib import Path
 from pydantic import BaseSettings
-# This shouldn't be necessary according to the docs, but without the load_dotenv it doesn't work at
-# least when running with python -m amcat4.config...
 from dotenv import load_dotenv
-load_dotenv()
 
 
 class Settings(BaseSettings):
     elastic_host: str = "http://localhost:9200"
+    middlecat_url: str = None
+    admin_password: str = None
+    config_file: Path = None
 
     class Config:
         env_prefix = "amcat4_"
 
 
+@functools.lru_cache()
 def get_settings():
-    settings = Settings()
+    # This shouldn't be necessary according to the docs, but without the load_dotenv it doesn't work at
+    # least when running with python -m amcat4.config...
+    temp = Settings()
+    load_dotenv(temp.config_file)
+    return Settings()
 
 
 if __name__ == '__main__':
-    for k, v in settings.dict().items():
+    # Echo the settings
+    for k, v in get_settings().dict().items():
         print(f"{Settings.Config.env_prefix.upper()}{k.upper()}={v}")
