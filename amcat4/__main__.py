bee5dda6bd14cb1543131f5e76cc2339127dbf1c
JBGruber <gruber@europa-uni.de>
2023-01-18 14:53:39 +0100

    added interactive config cli command

--- amcat4/__main__.py
+++ amcat4/__main__.py
@@ -9,7 +9,6 @@ import os
 import secrets
 import sys
 import urllib.request
-
 import uvicorn
 
 from amcat4 import index
@@ -46,15 +45,19 @@ def run(args):
     uvicorn.run("amcat4.api:app", host="0.0.0.0", reload=not args.nodebug, port=args.port)
 
 
+def base_env():
+    return dict(
+        amcat4_secret_key=secrets.token_hex(nbytes=32),
+        amcat4_middlecat_url="https://middlecat.up.netlify.app",
+    )
+
+
 def create_env(args):
     if os.path.exists('.env'):
         print('*** File .env already exists, quitting ***')
         sys.exit(1)
 
-    env = dict(
-        amcat4_secret_key=secrets.token_hex(nbytes=32),
-        amcat4_middlecat_host="https://middlecat.up.netlify.app",
-    )
+    env = base_env()
     if args.admin_email:
         env['amcat4_admin_email'] = args.admin_email
     if args.admin_password:
@@ -91,6 +94,60 @@ def list_users(_args):
         print("(No users defined yet, set AMCAT4_ADMIN_PASSWORD in environment use add-admin to add users by email)")
 
 
+def config_amcat(args):
+    settings = get_settings().dict()
+    # location of env should stay the same so it is found on restart
+    del settings["env_file"]
+    # if .env does not exist yet, secret_key should be created
+    if not os.path.exists('.env'):
+        settings["secret_key"] = base_env()["amcat4_secret_key"]
+    if args.name:
+        # some variables should not be changed by the user (currently just secret_key)
+        do_not_change = ["secret_key"] + list(filter(lambda x: x != args.name, settings.keys()))
+    else:
+        do_not_change = ["secret_key"]
+    for k, v in settings.items():
+        # some variables probably shouldn't be changed by the user (currently just secret_key)
+        if k not in do_not_change:
+            v = menu(k, v)
+            if v == "aborted":
+                settings = "aborted"
+                break
+            else:
+                if str(k) == "admin_email" and v is not None:
+                    set_global_role(v, Role.ADMIN)
+                settings[k] = v
+
+    if settings != "aborted":
+        with open(".env", 'w') as f:
+            for key, val in settings.items():
+                if val is not None:
+                    f.write(f"amcat4_{key}={val}\n")
+        os.chmod(".env", 0o600)
+        print(f"*** Changed {bold('.env')} file ***")
+
+
+def bold(x):
+    return '\033[1m' + str(x) + '\033[0m'
+
+
+def menu(k, v):
+    choice = input(f"The current value for {bold(k)} is {bold(v)}. Do you want to change it (Yes/{bold('No')}/Cancel)?")
+    # remain on same line to hide settings from onlookers
+    sys.stdout.write("\033[F\033[K")
+    if choice in ("No", "NO", "N", "n", ""):
+        return v
+    elif choice in ("Yes", "YES", "Y", "y"):
+        v = input(f"Enter new value for {k}:")
+        sys.stdout.write("\033[F\033[K")
+        return v
+    elif choice in ("Cancel", "CANCEL", "C", "c"):
+        return "aborted"
+    else:
+        print("Choose one of Yes/No/Cancel")
+        menu(k, v)
+
+
 parser = argparse.ArgumentParser(description=__doc__, prog="python -m amcat4")
 
 subparsers = parser.add_subparsers(dest="action", title="action", help='Action to perform:', required=True)
@@ -107,6 +164,9 @@ p.add_argument("-P", "--no-admin_password", action='store_true', help="Disable a
 
 p.set_defaults(func=create_env)
 
+p = subparsers.add_parser('config', help='Configure (all) settings in amcat4 in an interactive menu.')
+p.add_argument("-n", "--name", help="Name of a specific setting that should be changed.")
+p.set_defaults(func=config_amcat)
 
 p = subparsers.add_parser('add-admin', help='Add a global admin')
 p.add_argument("email", help="The email address of the admin user.")
