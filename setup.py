109b2799165310d722321570ee8c1f9d9221a8a0
JBGruber <johannesb.gruber@gmail.com>
2025-10-01 10:01:10 +0200

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name="amcat4",
-    version="4.0.16",
+    version="4.0.17",
     description="API for AmCAT4 Text Analysis",
     author="Wouter van Atteveldt",
     author_email="wouter@vanatteveldt.com",
